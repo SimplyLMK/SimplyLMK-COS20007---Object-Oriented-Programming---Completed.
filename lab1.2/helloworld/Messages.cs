1fc37be3ff94d8fa5b4b9aa6927d72c38d258931
SimplyLMK <129202743+SimplyLMK@users.noreply.github.com>
2023-05-12 09:09:21 +0700

    Add files via upload

---
+++ lab1.2/helloworld/Messages.cs
@@ -0,0 +1,27 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    public class Messages
+    {
+        private string _text;
+        public Messages(string text) 
+        {
+            {
+                _text = text;
+            }
+
+        }
+
+        public void Print()
+        {
+            Console.WriteLine(_text);
+        }
+
+    }
+}
---
+++ lab1.2/helloworld/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace HelloWorld
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+
+            Messages[] messages = new Messages[5];
+            messages[0] = new Messages("Welcome back!");
+            messages[1] = new Messages("What a lovely name");
+            messages[2] = new Messages("Great name");
+            messages[3] = new Messages("Oh hi!");
+            messages[4] = new Messages("That is a silly name");
+
+            
+            while (true)
+            {
+                Console.WriteLine("enter ur name");
+                string text = Console.ReadLine();
+
+                switch (text.ToLowerInvariant())
+                {
+                    case "kha":
+                        messages[0].Print();
+                        break;
+
+                    case "thinh":
+                        messages[1].Print();
+                        break;
+
+                    case "matthew mitchell":
+                        messages[2].Print();
+                        break;
+
+                    case "chris mccarthy":
+                        messages[3].Print();
+                        break;
+
+                    default:
+                        messages[4].Print();
+                        break;
+
+                }
+
+
+            }
+            
+        }
+    }
+}
