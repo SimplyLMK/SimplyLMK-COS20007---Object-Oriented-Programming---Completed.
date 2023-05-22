84c4fa03926eea8a45c428b59294933d14fad4cb
SimplyLMK <129202743+SimplyLMK@users.noreply.github.com>
2023-05-22 10:06:49 +0700

    Add files via upload

---
+++ lab7.2/CODE/Path.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Swin_ADV
+{
+    public class Pathing : Game_Object
+    {
+        bool _deadEnd;
+        Location _initial, _destination;
+
+        public Pathing(string[] idents, string name, string desc, Location initial, Location destination) : base(idents, name, desc)
+        {
+            _initial = initial;
+            _destination = destination;
+            _deadEnd = false;
+            AddIdentifier("path");
+
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < name.Length; i++)
+            {
+                if (name[i] == ' ')
+                {
+                    AddIdentifier(sb.ToString());
+                    sb.Clear();
+                }
+                else
+                {
+                    sb.Append(name[i]);
+                }
+            }
+            AddIdentifier(sb.ToString());
+        }
+
+        public Location Destination
+        {
+            get { return _destination; }
+        }
+
+        public override string Short_Description
+        {
+            get { return Name; }
+        }
+
+    }
+}
---
+++ lab7.2/CODE/Player.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Swin_ADV
+{
+    public class Player : Game_Object, I_Have_Inventory
+    {
+        private Inventory _inventory;
+        private Location _location;
+        Game_Object sth;
+
+        public Player(string name, string desc) : base(new string[] { "me", "inventory" }, name, desc)
+        {
+            _inventory = new Inventory();
+        }
+
+        public Inventory Inventory
+        {
+            get
+            {
+                return _inventory;
+            }
+            set
+            {
+                _inventory = value;
+            }
+        }
+
+        public Game_Object Locate(string id)
+        {
+            if (this.AreYou(id))
+            {
+                return this;
+            }
+             if (_inventory.Has_Item(id))
+            {
+                return _inventory.Fetch(id);
+            }
+
+            sth = _inventory.Fetch(id);
+
+            if (_location != null)
+            {
+                sth = _location.Locate(id);
+                return sth;
+            }
+            else
+            {
+                return null;
+            }
+           
+
+        }
+
+        public override string Full_Description
+        {
+            get
+            {
+                StringBuilder sb = new StringBuilder(base.Full_Description);
+                sb.Append("\nYou are carrying:\n");
+                sb.Append(_inventory.ItemList);
+                return sb.ToString();
+            }
+        }
+
+        public Location Location
+        {
+            get { return _location; }
+            set { _location = value; }
+        }
+
+        public void Move_Command(Pathing path)
+        {
+            if (path.Destination != null)
+            {
+                _location = path.Destination;
+            }
+        }
+    }
+
+}
